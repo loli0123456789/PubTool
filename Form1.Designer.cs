25a3cdf314932d8b2671cfe271315ed197b7d5f3
loli0123456789 <66709229+loli0123456789@users.noreply.github.com>
2023-12-12 16:48:01 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PubTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.fileFrom = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(134, 77);
+            this.tbFrom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(615, 28);
+            this.tbFrom.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "发布路径";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(748, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "…";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 208);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "提取发布文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(748, 138);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(29, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "…";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "提取路径";
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(134, 137);
+            this.tbTo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(615, 28);
+            this.tbTo.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(52, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(647, 108);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "说明：\r\n发布路径：是通过VS发布功能自动生成的文件路径；\r\n提取路径：是需要最终发布到服务器的文件路径。\r\n\r\n直接将该工具放到发布路径下，启动该工具即可自动默" +
+    "认发布路径、提取路径 。\r\n其中，提取路径自动默认为发布路径的父级目录下当前日期文件夹。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 540);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFrom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "发布工具V0.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.FolderBrowserDialog fileFrom;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PubTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //将程序直接放到发布路径下，自动识别
+            string appPath = Application.StartupPath;
+            tbFrom.Text = appPath;
+
+            DirectoryInfo directory = new DirectoryInfo(appPath);
+            string appPathName = directory.Name;
+
+            string parentPath = Directory.GetParent(appPath).FullName;
+            //在父级路径下建立当前日期的子文件夹，用于拷贝文件
+            string newPath = Path.Combine(parentPath, $"{DateTime.Now.ToString("yyyyMMdd")}_{appPathName}");
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            tbTo.Text = newPath;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fromPath = tbFrom.Text;
+            if (string.IsNullOrEmpty(fromPath))
+            {
+                MessageBox.Show("请选择发布路径");
+
+                return;
+            }
+            if (!Directory.Exists(fromPath))
+            {
+                MessageBox.Show("发布路径不存在");
+
+                return;
+            }
+
+            string toPath = tbTo.Text;
+            if (string.IsNullOrEmpty(toPath))
+            {
+                MessageBox.Show("请选择提取路径");
+
+                return;
+            }
+            if (!Directory.Exists(fromPath))
+            {
+                MessageBox.Show("提取路径不存在");
+
+                return;
+            }
+
+            try
+            {
+                GetAndCopyFile(fromPath, fromPath, toPath);
+
+                MessageBox.Show("获取并复制文件成功！");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("获取并复制文件出错", ex);
+            }
+        }
+
+        protected void GetAndCopyFile(string rootPath, string fromPath, string toPath)
+        {
+            string[] paths = Directory.GetDirectories(fromPath);
+            //递归遍历文件夹，去找文件
+            foreach (string path in paths)
+            {
+                GetAndCopyFile(rootPath, path, toPath);
+            }
+
+            //找文件夹下的文件
+            string[] filePaths = Directory.GetFiles(fromPath);
+            //遍历文件，进行拷贝
+            foreach (string filePath in filePaths)
+            {
+                FileInfo file = new FileInfo(filePath);
+                //当天修改文件，进行拷贝
+                if (file.LastWriteTime.ToShortDateString() == DateTime.Now.ToShortDateString())
+                {
+                    string fromFilePath = file.FullName;
+                    string newFIlePath = fromFilePath.Replace(rootPath, toPath);
+                    FileInfo newFile = new FileInfo(newFIlePath);
+                    if (!newFile.Directory.Exists)
+                    {
+                        newFile.Directory.Create();
+                    }
+                    file.CopyTo(newFIlePath, true);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileFrom.ShowDialog();
+
+            tbFrom.Text = fileFrom.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fileFrom.ShowDialog();
+
+            tbTo.Text = fileFrom.SelectedPath;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string flag = "出版,";
+            int dex = flag.IndexOf(null);
+            MessageBox.Show(dex.ToString());
+        }
+    }
+}
